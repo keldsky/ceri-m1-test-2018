de3eb06ae9c6269e8592a650e811b54a640a00b8
gaby <gaby@bzaby>
2018-04-16 22:10:36 +0200

    moi

--- test/fr/univavignon/rodeo/IAnimalTest.java
+++ test/fr/univavignon/rodeo/IAnimalTest.java
@@ -1,5 +1,21 @@
 package fr.univavignon.rodeo;
 
+import java.util.List;
+
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
 public class IAnimalTest {
 
+  @Test
+  public void test() {
+    List<String> mockList = mock(List.class);
+    mockList.add("First");
+    when(mockList.get(0)).thenReturn("Mockito");
+    when(mockList.get(1)).thenReturn("JCG");
+    assertEquals("Mockito", mockList.get(0));
+    assertEquals("JCG", mockList.get(1));
+  }
 }
