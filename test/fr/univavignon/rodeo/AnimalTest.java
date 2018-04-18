b41fb7de8b9f7c7d6e1631f8766842e5eefa2976
gaby <gaby@bzaby>
2018-04-18 02:31:09 +0200

    gab

--- test/fr/univavignon/rodeo/AnimalTest.java
+++ test/fr/univavignon/rodeo/AnimalTest.java
@@ -1,5 +1,47 @@
 package fr.univavignon.rodeo;
 
-public class AnimalTest {
+import java.util.List;
 
+import org.junit.Test;
+
+import fr.univavignon.rodeo.api.IAnimal;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import static org.junit.Assert.*;
+
+public class AnimalTest {
+	
+	@Test
+	public void testXP() {
+		IAnimal ia = mock(IAnimal.class);
+		when(ia.getXP()).thenReturn(20);
+		assertEquals(20 , ia.getXP());	
+	}
+	
+	@Test
+	public void testSecret() {
+		IAnimal ia = mock(IAnimal.class);
+		when(ia.isSecret()).thenReturn(true);
+		assertEquals(true , ia.isSecret());	
+	}
+	
+	@Test
+	public void testEndangered() {
+		IAnimal ia = mock(IAnimal.class);
+		when(ia.isEndangered()).thenReturn(true);
+		assertEquals(true , ia.isEndangered());	
+	}
+	
+	@Test
+	public void testBoss() {
+		IAnimal ia = mock(IAnimal.class);
+		when(ia.isBoss()).thenReturn(true);
+		assertEquals(true , ia.isBoss());	
+	}
+	
+	
+    
+  
 }
--- test/fr/univavignon/rodeo/EnvironmentTest.java
+++ test/fr/univavignon/rodeo/EnvironmentTest.java
@@ -1,5 +1,16 @@
 package fr.univavignon.rodeo;
 
+import java.util.List;
+
+import org.junit.Test;
+
+import fr.univavignon.rodeo.api.IEnvironment;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import static org.junit.Assert.*;
+
 public class EnvironmentTest {
 
 }
