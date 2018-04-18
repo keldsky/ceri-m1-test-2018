066618571879040259233b3d3ad33f47fdbb2a8f
gaby <gaby@10.20.12.237>
2018-04-18 15:37:38 +0200

    tst

--- test/fr/univavignon/rodeo/AnimalTest.java
+++ test/fr/univavignon/rodeo/AnimalTest.java
@@ -2,6 +2,7 @@ package fr.univavignon.rodeo;
 
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import fr.univavignon.rodeo.api.IAnimal;
@@ -13,31 +14,51 @@ import static org.junit.Assert.*;
 
 public class AnimalTest {
 	
+	IAnimal ia;
+	
+	public static IAnimal getInstance()
+	{
+		IAnimal ia = mock(IAnimal.class);
+		
+		when(ia.getXP()).thenReturn(20);
+		when(ia.isSecret()).thenReturn(true);
+		when(ia.isEndangered()).thenReturn(true);
+		when(ia.isBoss()).thenReturn(true);
+		
+		return ia;
+	}
+	
+	@Before
+	public void Instance()
+	{
+		ia = mock(IAnimal.class);
+		when(ia.getXP()).thenReturn(20);
+		when(ia.isSecret()).thenReturn(true);
+		when(ia.isEndangered()).thenReturn(true);
+		when(ia.isBoss()).thenReturn(true);
+	}
+	
 	@Test
 	public void testXP() {
-		IAnimal ia = mock(IAnimal.class);
-		when(ia.getXP()).thenReturn(20);
+		
 		assertEquals(20 , ia.getXP());	
 	}
 	
 	@Test
 	public void testSecret() {
-		IAnimal ia = mock(IAnimal.class);
-		when(ia.isSecret()).thenReturn(true);
+		
 		assertEquals(true , ia.isSecret());	
 	}
 	
 	@Test
 	public void testEndangered() {
-		IAnimal ia = mock(IAnimal.class);
-		when(ia.isEndangered()).thenReturn(true);
+		
 		assertEquals(true , ia.isEndangered());	
 	}
 	
 	@Test
 	public void testBoss() {
-		IAnimal ia = mock(IAnimal.class);
-		when(ia.isBoss()).thenReturn(true);
+	
 		assertEquals(true , ia.isBoss());	
 	}
 	
--- test/fr/univavignon/rodeo/SpecieTest.java
+++ test/fr/univavignon/rodeo/SpecieTest.java
@@ -1,5 +1,56 @@
 package fr.univavignon.rodeo;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import fr.univavignon.rodeo.api.IAnimal;
+import fr.univavignon.rodeo.api.ISpecie;
+
 public class SpecieTest {
+	
+	
+	ISpecie is;
+	List li;
+	@Before
+	public void Instance()
+	{
+		is = mock(ISpecie.class);
+		
+		when(is.getArea()).thenReturn(10);
+		
+		IAnimal ia = AnimalTest.getInstance();
+		
+		List<IAnimal> la = new ArrayList();
+		
+		la.add(ia);
+		
+		li = new ArrayList();
+		
+		li.add("a");
+		li.add("b");
+		li.add("c");
+		
+		when(is.getAnimals()).thenReturn(li);
+		
+	}
+	
+	@Test
+	public void testArea() {
+		
+		assertEquals(10 , is.getArea());	
+	}
+	
+	@Test
+	public void testAnimals() {
+		
+		assertEquals(li , is.getAnimals());	
+	}
 
 }
